f6e533f631e38fba1522ff1a9760df047d25f551
NILEG <104659110+NILEG@users.noreply.github.com>
2023-10-30 19:59:16 +0500

    Update main.cpp

--- Double Circular Queue/main.cpp
+++ Double Circular Queue/main.cpp
@@ -5,15 +5,12 @@ int main()
 	c1.enqueue_right(10);
 	c1.enqueue_right(20);
 	
-	c1.dequeue_right();
-	c1.dequeue_left();
+	c1.enqueue_right(30);
+	c1.enqueue_right(40);
 	
-	c1.enqueue_right(10);
-	c1.enqueue_right(20);
-	
-	c1.enqueue_left(30);
-	c1.enqueue_left(40);
-	
+	c1.enqueue_left(9);
+	c1.enqueue_left(8);
+	c1.reverse();
 	c1.display();
 	return 0;
 }
