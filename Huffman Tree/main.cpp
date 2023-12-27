6136949e00bde642a2dde4e7478956ec1f4cd7de
NILEG <104659110+NILEG@users.noreply.github.com>
2023-12-27 19:40:09 +0500

    Add files via upload

--- Huffman Tree/main.cpp
+++ Huffman Tree/main.cpp
@@ -1,54 +1,28 @@
 #include "my_header.h"
-#include<graphics.h>
 #include<iostream>
-#include<string>
+#include <string>
+#include<graphics.h>
 using namespace std;
-void create(node *ptr, int x1, int y1, int x2, int y2, int level)
-{
-	if(ptr ==NULL)
-	{
-		return ;
-	}
-	else
-	{
-		
-		//Creating Line
-		setcolor(YELLOW);
-	    line(x1, y1, x2, y2);
-	    //Creating  Circle
-		setcolor(WHITE);
-		setfillstyle(SOLID_FILL,RED);
-		circle(x1, y1, 30);
-		floodfill(x1,y1,WHITE);
-		//Putting Text
-		if(ptr->sub_left == NULL && ptr->sub_right == NULL)
-		{
-			settextstyle(8, 0, 4);
-			setcolor(WHITE);
-			setbkcolor(RED);
-			char* char_arr = &ptr->name[0]; 
-			outtextxy(x1-10, y1-15,char_arr);
-		}
-		//Recursion
-	    create(ptr->sub_left, x1-(30*level), y1+80, x1, y1, level-1);
-	    create(ptr->sub_right, x1+(30*level), y1+80, x1, y1, level-1);
-	}
-}
 int main()
 {
-	initwindow(1200,800);
-	int x1=250, y1=100, x2=250, y2=100;
 	control c1;
-	c1.insert(5, "A", NULL, NULL);
-	c1.insert(9, "B", NULL, NULL);
-	c1.insert(12, "C", NULL, NULL);
-	c1.insert(4, "D", NULL, NULL);
-	c1.insert(23, "E", NULL, NULL);
-	c1.insert(46, "F", NULL, NULL);
-	c1.level_order_traversel();
+	node *ptr=NULL; 
+	c1.window1=initwindow(getmaxwidth(),getmaxheight()-10);
+	/*c1.insert(c1.main_root, 5, "A", NULL, NULL, 'm');
+	c1.insert(c1.main_root, 9, "B", NULL, NULL, 'm');
+	c1.insert(c1.main_root, 45, "F", NULL, NULL,'m');
+	c1.insert(c1.main_root, 12, "C", NULL, NULL,'m');
+	c1.insert(c1.main_root, 13, "D", NULL, NULL,'m');
+	c1.insert(c1.main_root, 16, "E", NULL, NULL,'m');*/
+	//c1.level_order_traversel();
+	//c1.show_top_queue();
+	//int counter=0;
+	//c1.total_item(c1.temp_root, counter);
+	//cout<<"TEMP: "<<counter<<endl;
+	//c1.show_top_queue();
 	c1.make_huffman_tree();
-	c1.show_codes(c1.root, "");
-	create(c1.root, x1,y1,x2,y2, c1.get_huffman_height(c1.root));
+	//c1.show_codes(c1.main_root, "");
+	cout<<"Waiting for key";
 	getch();
 	closegraph();
 }
